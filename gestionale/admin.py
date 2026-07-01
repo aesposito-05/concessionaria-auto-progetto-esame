2e2f41c389d158eb37efac2bbaf2507112e59bb4
Antonio Esposito <antonio@example.com>
2026-07-01 11:42:22 +0200

    Rimuove file non necessari e semplifica admin
    - Eliminati 5 template orfani (registrazione, storico vendite/manutenzioni)
    - Eliminato popola_db.py
    - admin.py ridotto a 2 modelli (Marca, Auto)

--- gestionale/admin.py
+++ gestionale/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 
-from .models import (Auto, Azienda, Cliente, DettaglioVendita, Dipendente,
-                     Fornitore, Manutenzione, Marca, Privato, Vendita)
+from .models import Auto, Marca
 
 admin.site.register(Marca)
-admin.site.register(Fornitore)
 admin.site.register(Auto)
-admin.site.register(Cliente)
-admin.site.register(Privato)
-admin.site.register(Azienda)
-admin.site.register(Dipendente)
-admin.site.register(Vendita)
-admin.site.register(DettaglioVendita)
-admin.site.register(Manutenzione)
