169d30e8875cb3ccfdf504c7dbb6675fee038478
Antonio Esposito <antonio@example.com>
2026-07-01 11:50:17 +0200

    Rimuove file di configurazione locali dal repository

--- avvia_concessionaria.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Avvio server Concessionaria Auto...
-cd /d "C:\Users\Utente\Desktop\progetto_concessionaria_parziale"
-call venv\Scripts\activate
-start "" "http://127.0.0.1:8000/"
-start "" "http://127.0.0.1:8000/admin/"
-python manage.py runserver
